36ee31fb762372ecdcc2c132b2cecb669b16818d
NP6 <np6.github@gmail.com>
2015-07-09 14:20:51 +0200

    JAVA getTargetFromUnicity
    JAVA getTargetFromUnicity
    version 1.0

---
+++ cookbook/java/README.md
@@ -0,0 +1,19 @@
+Cook Book for java Implementation
+==
+
+
+getTargetFromUnicity
+--
+
+This code example allows you to get the informations of a target (Guid, fields....following the model you can find on API documentation : https://v8.mailperformance.com/doc) giving his unicity criteria.
+
+Coming soon 
+--
+
+Webhooks to listen clicks and openings.
+
+
+Version
+--
+
+1.0 
---
+++ cookbook/java/getTargetFromUnicity.java
@@ -0,0 +1,53 @@
+package getTargetFromUnicity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class getTargetFromUnicity {
+
+	public static void main(String[] args) throws IOException
+	{
+		//Ici, renseignez l'email dont vous voulez obtenir les valeurs des champs
+		String unicity = "test@test.com";
+		
+		//Lancement de la connection pour remplir la requete
+		String url = "http://v8.mailperformance.com/targets?unicity=" + unicity;
+		HttpURLConnection con = (HttpURLConnection)new URL(url).openConnection();
+		con.setRequestMethod("GET");
+		con.setDoInput(true);
+        con.setDoOutput(true);
+		
+		//Mise en place du xKey et des options
+		con.setRequestProperty("X-Key", "ABCDEFJHIJKLMNOPQRSTUVWXYZ0123456789");
+		con.setRequestProperty("Content-Type", "application/json");
+		
+		//Verification des reponses
+        String responseMessage = con.getResponseMessage();
+        int responseCode = con.getResponseCode();
+        if (responseCode != 200 && responseCode != 201)
+        {
+        	//Affichage de l'erreur
+        	System.out.print("Error : " + Integer.toString(responseCode) + ' ' + responseMessage);
+        }
+        else
+        {
+        	//Lecture des donnees ligne par lignes
+        	BufferedReader buffRead = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        	StringBuilder reply = new StringBuilder();
+            String line;
+            while ((line = buffRead.readLine()) != null)
+            {
+            	reply.append(line + "\n");
+            }
+            buffRead.close();
+            
+            //Affichage des donnees
+        	System.out.print(reply);
+        }
+        
+        con.disconnect();
+	}
+}
