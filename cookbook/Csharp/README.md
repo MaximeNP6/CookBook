2e81259c9a31fe96a4819db1df319c8c290c9836
NP6 <np6.github@gmail.com>
2015-07-13 15:38:18 +0200

    Add Csharp
    Add getTargetFromUnicity.cs
    Add getTargetAndSendMessage.cs
    Add README for Csharp

---
+++ cookbook/Csharp/README.md
@@ -0,0 +1,24 @@
+Cook Book for Csharp Implementation
+==
+
+
+getTargetFromUnicity
+--
+
+This code example allows you to get the informations of a target (Guid, fields....following the model you can find on API documentation : https://v8.mailperformance.com/doc) giving his unicity criteria.
+
+getTargetAndSendMessage
+--
+
+This code takes the id of a target (with the getTargetFromUnicity's informations) and send it a pre-created message (the idMessage) in your account.
+To use this code you must import the JSON.Net library : How install Java JSON.Net library on Visual Studio : -> Right click the Project -> Manage NuGet Packages... -> Configure build path -> Search Online (Ctrl + E) "Json.NET" ->  -> Install "Json.NET" -> Close
+
+Coming soon
+--
+Webhooks to listen clicks and openings.
+
+
+Version
+--
+
+1.0 
---
+++ cookbook/Csharp/getTargetAndSendMessage.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace getTargetAndSendMessage
+{
+    class Program
+    {
+        static HttpWebRequest Connect(string url, string xKey, string method)
+        {
+            //Lancement de la connection pour remplir la requete
+            HttpWebRequest con = (HttpWebRequest)WebRequest.Create(url);
+            con.Method = method;
+
+            //Mise en place du xKey et des options
+            con.Headers.Add("X-Key", xKey);
+            con.ContentType = "application/json";
+
+            return (con);
+        }
+
+        static void Main(string[] args)
+        {
+            //Ici, renseignez l'email dont vous voulez obtenir les valeurs des champs, la X-Key et l'id du message a envoyer
+            string unicity = "test@test.com";
+            string xKey = "ABCDEFJHIJKLMNOPQRSTUVWXYZ0123456789";
+            string idMessage = "000ABC";
+
+            //Appel de la fonction Connect
+            string url = "http://v8.mailperformance.com/targets?unicity=" + unicity;
+            HttpWebRequest con = Connect(url, xKey, "GET");
+            
+            //Verification des reponses
+            HttpWebResponse httpResponseGet = (HttpWebResponse)con.GetResponse();
+            if ((int)httpResponseGet.StatusCode != 200)
+            {
+                //Affichage de l'erreur
+                Console.Write("Error : {0} {0}", (int)httpResponseGet.StatusCode, httpResponseGet.StatusCode.ToString());
+            }
+            else
+            {
+                //Lecture des donnees
+                StreamReader reader = new StreamReader(con.GetResponse().GetResponseStream());
+                string responseString = reader.ReadToEnd();
+                httpResponseGet.Close();
+                reader.Close();
+
+                //On prend le idTarget
+                dynamic responseJson = JsonConvert.DeserializeObject(responseString);
+                string idTarget = responseJson.id;
+
+                //Nouvelle url en fonction de l'id du message et de la cible
+                url = "http://v8.mailperformance.com/actions/" + idMessage + "/targets/" + idTarget;
+
+                //Lancement de la connection pour remplir la requete
+                con = Connect(url, xKey, "POST");
+                con.ContentLength = 0;
+
+                //Verification des reponses
+                HttpWebResponse httpResponsePost = (HttpWebResponse)con.GetResponse();
+                if ((int)httpResponsePost.StatusCode != 204)
+                {
+                    //Affichage de l'erreur
+                    Console.Write("Error : {0} {0}", (int)httpResponsePost.StatusCode, httpResponsePost.StatusCode.ToString());
+                }
+                else
+                {
+                    Console.Write("Message sent to " + unicity);
+                }
+                httpResponsePost.Close();
+            }
+
+            //Attente de lecture (optionnel)
+            Console.ReadLine();
+
+            httpResponseGet.Close();
+        }
+    }
+}
