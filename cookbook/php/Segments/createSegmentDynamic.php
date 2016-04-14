690f7be119ca91cec199e13bf82fef8db9c94629
NP6 <np6.github@gmail.com>
2016-04-14 09:58:13 +0200

    Refacto create Segment for PHP

--- cookbook/php/Segments/createSegmentDynamic.php
+++ cookbook/php/Segments/createSegmentDynamic.php
@@ -1,90 +1,83 @@
 <?php
 
-//Ici, renseignez la xKey et les parametres personnalises
-$xKey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-$segmentId = 0123;	//Id du segment a modifier ('null' si le segment doit etre creer)
+/**
+ * Ce script permet de créer un Segment Dynamique.
+ *
+ * @package cookbook
+ */
 
-$type = 'dynamic';	//Code pour creer un segment Dynamique
-$name = 'SegmentDynamic (php)';	//Nom du segment
-$description = 'SegmentDynamic (php)';	//Description du segment
-$expiration = '2016-01-08T12:11:00Z';	//Date d'expiration du segment
-$isTest = true;	//Segment de test : oui = 'true' / non = 'false'
-$parentId = null;	//Id du segment pere ('null' pour aucun segments pere)
+require __DIR__ . '/../utils.php';
 
-//On trouve l'adresse pour la requete
-$url = 'http://v8.mailperformance.com/segments/' . $segmentId;
+/**
+ * Variable contenant les configurations pour se connecter à l'API.
+ *
+ * @var array
+ */
+$configs = parse_ini_file(__DIR__. '/../config.ini');
+/**
+ * Id du segment à modifier. ('null' si le segment doit être créé)
+ *
+ * @var integer
+ */
+$segmentId = null;
+/**
+ * Code pour créer un segment Statique.
+ *
+ * @var string
+ */
+$type = 'dynamic';
+/**
+ * Nom du segment.
+ *
+ * @var string
+ */
+$name = 'SegmentDynamic (php)';
+/**
+ * Description du segment.
+ *
+ * @var string
+ */
+$description = 'SegmentDynamic (php)';
+/**
+ * Date d'expiration du segment.
+ *
+ * @var string
+ */
+$expiration = '2026-08-08T12:11:00Z';
+/**
+ * Défini si le Segment est un Segment de test ou non.
+ *
+ * @var bool
+ */
+$isTest = true;
 
-//Creation du Json du message
+// Creation du Json du message
 $arr = array(
-    'type' => $type,
-    'name' => $name,
-    'description' => $description,
-    'expiration' => $expiration,
-    'isTest' => $isTest,
-    'parentId' => $parentId);
+  'type'        => $type,
+  'name'        => $name,
+  'description' => $description,
+  'expiration'  => $expiration,
+  'isTest'      => $isTest
+);
+// On affiche le message
+$dataJson = json_encode($arr);
+echo $dataJson . "\n";
 
-
-//On affiche le message
-$message = json_encode($arr);
-echo $message . "\n";
-
-//Connexion
-if ($segmentId != null)
-    $con = connect($url, $xKey, $message, 'PUT');
-else
-    $con = connect($url, $xKey, $message, 'POST');
-
-$result = $con['result'];
-$info = $con['info'];
-$req = $con['req'];
-
-if ($info['http_code'] != 200)
-{
-    echo 'Error : ' . $info['http_code'];
+// Connexion
+$url = $configs['url'] . 'V1/segments/' . $segmentId;
+if ($segmentId != null) {
+  $con = connect($url, $configs['xKey'], $dataJson, 'PUT');
 }
-else
-{
-    //Le segment a bien ete change
-    echo "\nThe segment dynamic  : " . $name . " is update.\n\n";
-}
-curl_close($req);
-
-
-
-//Fonctions -----
-
-
-
-//Utilisation de cURL pour remplir les requetes
-function startCurlInit($url)
-{
-    $init = curl_init();
-    curl_setopt($init, CURLOPT_URL, $url);
-    curl_setopt($init, CURLOPT_RETURNTRANSFER, true);
-    return ($init);
+else {
+  $con = connect($url, $configs['xKey'], $dataJson, 'POST');
 }
 
-//Fonction de connexion
-function connect($url, $xKey, $message, $method)
-{
-    //On remplit la requete
-    $req = startCurlInit($url);
-    curl_setopt($req, CURLOPT_CUSTOMREQUEST, $method);
-
-    //Mise en place du xKey et des options
-    curl_setopt($req, CURLOPT_HTTPHEADER, array(
-    'X-Key: ' . $xKey,
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($message)));
-    curl_setopt($req, CURLOPT_POSTFIELDS, $message);
-
-    //Execution de la requete
-    $result = curl_exec($req);
-
-    //Verification des reponses
-    $info = curl_getinfo($req);
-
-    return (array('result' => $result, 'info' => $info, 'req' => $req));
+if ($con['info']['http_code'] == 200) {
+  // Le segment a bien ete cree
+  echo 'The segment dynamic  : ' . $name . " has been created / updated.\n\n";
+}
+else {
+  echo 'Error : ' . $con['info']['http_code'];
 }
 
 ?>
