e78c76c419f5f34f7c4a08f87eff1b6330309877
NP6 <np6.github@gmail.com>
2016-04-15 12:33:58 +0200

    Refacto of AutoImport and getImport

--- cookbook/Csharp/Imports/createAutoImports.cs
+++ cookbook/Csharp/Imports/createAutoImports.cs
@@ -1,34 +1,30 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json.Linq;
 
 namespace createAutoImports
 {
-    class Program
+    internal class Program
     {
         public static JObject getCreateImportJson()
         {
             // Remplissez les informations obligatoires
-            string importName = "Nom de votre Import automatique"; // Nom de l'import
-            string schedulerName = "Nom du scheduler"; // Nom du scheduler
-            int binding = 1234; // Id du binding
-            int segmentId = 1234; // Id du segment
-            int fieldsId = 1234; // Id du field
-            string[] contactsId = { "1234ABCD" }; // Id des utilisateurs : Administration -> Utilisateurs -> Identifiant
-            int[] groupsContactsId = { 1234 }; // Id des groupes : Administration -> Groupes -> Identifiant
+            const string importName = "Nom de votre Import automatique"; // Nom de l'import
+            const string schedulerName = "Nom du scheduler"; // Nom du scheduler
+            const int binding = 8335; // Id du binding
+            const int segmentId = 14091; // Id du segment
+            const int fieldsId = 1234; // Id du field
+            string[] contactsId = { "044EAE5A" }; // Id des utilisateurs : Administration -> Utilisateurs -> Identifiant
+            int[] groupsContactsId = {  }; // Id des groupes : Administration -> Groupes -> Identifiant
 
             //Creation du Json
 
 
-            JObject source = new JObject();
+            var source = new JObject();
             source.Add("type", "ftp");
 
-            JObject occurs = new JObject();
+            var occurs = new JObject();
             occurs.Add("type", "daily"); //Quotidient
             /* Autres possibilites :
             occurs.Add("type", "weekly"); // Hebdomadaire
@@ -40,20 +36,20 @@ namespace createAutoImports
             occurs.Add("days", days);
             */
 
-            JObject periodicityValue = new JObject();
+            var periodicityValue = new JObject();
             periodicityValue.Add("hour", 6);
             periodicityValue.Add("minute", 0);
             periodicityValue.Add("second", 0);
 
-            JObject periodicity = new JObject(); // Heure de l'import
+            var periodicity = new JObject(); // Heure de l'import
             periodicity.Add("type", "once");
             periodicity.Add("value", periodicityValue);
 
-            JObject frequency = new JObject();
+            var frequency = new JObject();
             frequency.Add("occurs", occurs);
             frequency.Add("periodicity", periodicity);
 
-            JObject validityStart = new JObject(); // Les dates doivent etre coherentes avec la date de creation
+            var validityStart = new JObject(); // Les dates doivent etre coherentes avec la date de creation
             validityStart.Add("year", 2016);
             validityStart.Add("month", 12);
             validityStart.Add("date", 1);
@@ -61,7 +57,7 @@ namespace createAutoImports
             validityStart.Add("minute", 0);
             validityStart.Add("second", 0);
 
-            JObject validityEnd = new JObject(); // Les dates doivent etre coherentes avec la date de creation
+            var validityEnd = new JObject(); // Les dates doivent etre coherentes avec la date de creation
             validityEnd.Add("year", 2017);
             validityEnd.Add("month", 10);
             validityEnd.Add("date", 1);
@@ -69,54 +65,54 @@ namespace createAutoImports
             validityEnd.Add("minute", 0);
             validityEnd.Add("second", 0);
 
-            JObject validity = new JObject(); // La validation n'est pas obligatoire
+            var validity = new JObject(); // La validation n'est pas obligatoire
             validity.Add("start", validityStart);
             validity.Add("end", validityEnd);
 
-            JObject scheduler = new JObject();
+            var scheduler = new JObject();
             scheduler.Add("type", "periodic");
             scheduler.Add("name", schedulerName);
             scheduler.Add("frequency", frequency);
             scheduler.Add("validity", validity);
 
-            JArray country = new JArray();
+            var country = new JArray();
             country.Add("FRA"); // Pour changer de pays de normalisation utiliser l'ISO ALPHA-3 Code (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3)
 
-            JObject fields = new JObject();
+            var fields = new JObject();
             fields.Add(fieldsId.ToString(), country);
 
-            JObject normalization = new JObject(); // Parametre de normalisation
+            var normalization = new JObject(); // Parametre de normalisation
             normalization.Add("type", "normalization");
             normalization.Add("fields", fields);
 
-            JObject segmentation = new JObject(); // Ajout des cibles a un Segment
+            var segmentation = new JObject(); // Ajout des cibles a un Segment
             segmentation.Add("type", "segmentation");
             segmentation.Add("segmentId", segmentId);
             segmentation.Add("emptyExisitingSegment", true);
 
-            JObject destination = new JObject();
+            var destination = new JObject();
             destination.Add("sms", true);
             destination.Add("email", true);
 
-            JObject redList = new JObject(); // Ajout des cibles a la liste rouge
+            var redList = new JObject(); // Ajout des cibles a la liste rouge
             redList.Add("type", "redList");
             redList.Add("destination", destination);
 
-            JObject rules = new JObject();
+            var rules = new JObject();
             rules.Add("ignore", true); //ou rules.Add("first", true); ou rules.Add("last", true);
 
-            JObject duplicate = new JObject(); // Regles a appliquer en cas de doublon
+            var duplicate = new JObject(); // Regles a appliquer en cas de doublon
             duplicate.Add("type", "duplicate");
             duplicate.Add("rules", rules);
 
 
-            JArray contactGuids = new JArray();
+            var contactGuids = new JArray();
             contactGuids.Add(contactsId);
 
-            JArray groupIds = new JArray();
+            var groupIds = new JArray();
             groupIds.Add(groupsContactsId);
 
-            JObject report = new JObject(); // Parametres du rapport
+            var report = new JObject(); // Parametres du rapport
             report.Add("type", "report");
             report.Add("sendFinalReport", false);
             report.Add("sendErrorReport", true);
@@ -124,20 +120,20 @@ namespace createAutoImports
             report.Add("contactGuids", contactGuids); // Id des utilisateurs : Administration -> Utilisateurs -> Identifiant
             report.Add("groupIds", groupIds); // Id des groupes : Administration -> Groupes -> Identifiant
 
-            JObject database = new JObject(); // Parametres pour mettre a jour une cible dans la base de donnees
+            var database = new JObject(); // Parametres pour mettre a jour une cible dans la base de donnees
             database.Add("type", "database");
             database.Add("updateExisting", true);
             database.Add("crushData", true);
 
-            JArray features = new JArray(); // Si vous voulez enlever une option il suffit de mettre la ligne en commentaire
-            features.Add(normalization);
+            var features = new JArray(); // Si vous voulez enlever une option il suffit de mettre la ligne en commentaire
+            //features.Add(normalization);
             features.Add(segmentation);
              //features.Add(redList);
             features.Add(duplicate);
             features.Add(report);
             features.Add(database);
 
-            JObject createImportJson = new JObject();
+            var createImportJson = new JObject();
             createImportJson.Add("name", importName);
             createImportJson.Add("binding", binding);
             createImportJson.Add("source", source);
@@ -150,123 +146,34 @@ namespace createAutoImports
             return (createImportJson);
         }
 
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////FIN DE CREATION DES JSONS///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////DEBUT DU PROGRAMME//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //Ne pas remplir
-            Object[] result = null;
-            HttpWebRequest con = null;
-            HttpWebResponse httpResponse = null;
+            // Changez le Path pour correspondre à la destination de votre fichier de configuration
+            var doc = new XmlDocument();
+            doc.Load("config.xml");
 
-
-            //Url de base
-            String urlBase = "http://v8.mailperformance.com/";
-            //X-Key
-            String xKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var xKey = doc.DocumentElement.SelectSingleNode("/config/xKey").InnerText;
+            var baseUrl = doc.DocumentElement.SelectSingleNode("/config/url").InnerText;
 
             //Lancement de la connexion pour remplir la requete
-            String url = urlBase + "imports/";
+            var url = baseUrl + "imports/";
 
             Console.Write("CREATE IMPORT\n");
 
-            result = allConnection(con, url, xKey, getCreateImportJson(), "POST");
-            httpResponse = (HttpWebResponse)result[2];
-            verification(result);
+            var result = Utils.allConnection(url, xKey, getCreateImportJson(), "POST");
+            //Verification des reponses
+            if ((int)result[0] == 200)
+            {
+                Console.Write("{0}", (string)result[3]);
+            }
+            else
+            {
+                //Affichage de l'erreur
+                Console.Write("Error : {0} {1}", (int)result[0], ((HttpWebResponse)result[2]).StatusCode.ToString());
+            }
 
             Console.ReadLine();
-            httpResponse.Close();
-        }
-
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////FIN DU PROGRAMME////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////DEBUT DES FONCTIONS/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-        static Boolean verification(Object[] result)
-        {
-            int response = 0;
-            HttpWebRequest con = null;
-            HttpWebResponse httpResponse = null;
-            String responseString = null;
-
-            response = (int)result[0];
-            con = (HttpWebRequest)result[1];
-            httpResponse = (HttpWebResponse)result[2];
-            responseString = (String)result[3];
-
-            if (response >= 200 && response <= 299)
-            {
-                //On affiche la reponse
-                Console.Write("OK : \n\n" + responseString + "\n\n");
-                return (true);
-            }
-            Console.Write("Error : " + response + " " + responseString + "\n");
-            return (false);
-        }
-
-        //Fonction de connexion
-        static HttpWebRequest Connect(String url, String xKey, String method)
-        {
-            //Lancement de la connexion pour remplir la requete
-            HttpWebRequest con = (HttpWebRequest)WebRequest.Create(url);
-            con.Method = method;
-
-            //Mise en place du xKey et des options
-            con.Headers.Add("X-Key", xKey);
-            con.ContentType = "application/json";
-            con.Accept = "application/vnd.mperf.v8.import.v1+json";
-
-            return (con);
-        }
-
-        //Fonction de connexion et envoie des informations
-        static Object[] allConnection(HttpWebRequest con, String url, String xKey, JObject jsonMessage, String method)
-        {
-            con = Connect(url, xKey, method);
-            con.ContentLength = 0;
-            if (jsonMessage != null)
-            {
-                con.ContentLength = jsonMessage.ToString().Length;
-                var streamWriter = new StreamWriter(con.GetRequestStream());
-                streamWriter.Write(jsonMessage);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-
-            //Test de l'envoi
-            HttpWebResponse httpResponse = null;
-            int response = 0;
-            String responseString = null;
-            try
-            {
-                httpResponse = (HttpWebResponse)con.GetResponse();
-                response = 200;
-
-                //Lecture des donnees
-                StreamReader reader = new StreamReader(con.GetResponse().GetResponseStream());
-                responseString = reader.ReadToEnd();
-                httpResponse.Close();
-                reader.Close();
-            }
-            //Reception du signal
-            catch (WebException ex)
-            {
-                if (ex.Status == WebExceptionStatus.ProtocolError)
-                {
-                    httpResponse = (HttpWebResponse)ex.Response;
-                    response = (int)httpResponse.StatusCode;
-                }
-            }
-            Object[] result = { response, con, httpResponse, responseString };
-            return (result);
+            ((HttpWebResponse)result[2]).Close();  
         }
     }
 }
--- cookbook/Csharp/Imports/getImports.cs
+++ cookbook/Csharp/Imports/getImports.cs
@@ -1,123 +1,38 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
+﻿using System;
 using System.Net;
+using System.Xml;
 
 namespace getImports
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //Ne pas remplir
-            Object[] result = null;
-            HttpWebRequest con = null;
-            HttpWebResponse httpResponse = null;
+            // Changez le Path pour correspondre à la destination de votre fichier de configuration
+            var doc = new XmlDocument();
+            doc.Load("config.xml");
 
-            //Url de base
-            String urlBase = "http://v8.mailperformance.com/";
-            //X-Key
-            String xKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var xKey = doc.DocumentElement.SelectSingleNode("/config/xKey").InnerText;
+            var baseUrl = doc.DocumentElement.SelectSingleNode("/config/url").InnerText;
 
-            //Lancement de la connexion pour remplir la requete
-            String url = urlBase + "imports/";
+            // Lancement de la connexion pour remplir la requete
+            var url = baseUrl + "imports/";
 
-            result = allConnection(con, url, xKey, null, "GET");
-            con = (HttpWebRequest)result[1];
-            httpResponse = (HttpWebResponse)result[2];
+            var result = Utils.allConnection(url, xKey, null, "GET");
 
-            verification(result);
+            //Verification des reponses
+            if ((int)result[0] == 200)
+            {
+                Console.Write("{0}", (string)result[3]);
+            }
+            else
+            {
+                //Affichage de l'erreur
+                Console.Write("Error : {0} {1}", (int)result[0], ((HttpWebResponse)result[2]).StatusCode.ToString());
+            }
 
             Console.ReadLine();
-            httpResponse.Close();
-        }
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////FIN DU PROGRAMME////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////DEBUT DES FONCTIONS/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        static Boolean verification(Object[] result)
-        {
-            int response = 0;
-            HttpWebRequest con = null;
-            HttpWebResponse httpResponse = null;
-            String responseString = null;
-
-            response = (int)result[0];
-            con = (HttpWebRequest)result[1];
-            httpResponse = (HttpWebResponse)result[2];
-            responseString = (String)result[3];
-
-            // Verification
-            if (response >= 200 && response <= 299)
-            {
-                // Tout est bon
-
-                //On affiche la reponse
-                Console.Write("OK : \n\n" + responseString + "\n\n");
-                return (true);
-            }
-            // Il y a eu une erreur
-            Console.Write("Error : " + response + " " + responseString + "\n");
-            return (false);
-        }
-
-        //Fonction de connexion
-        static HttpWebRequest Connect(String url, String xKey, String method)
-        {
-            //Lancement de la connexion pour remplir la requete
-            HttpWebRequest con = (HttpWebRequest)WebRequest.Create(url);
-            con.Method = method;
-
-            //Mise en place du xKey et des options
-            con.Headers.Add("X-Key", xKey);
-            con.ContentType = "application/json";
-            con.Accept = "application/vnd.mperf.v8.import.v1+json";
-
-            return (con);
-        }
-
-        //Fonction de connexion et envoie des informations
-        static Object[] allConnection(HttpWebRequest con, String url, String xKey, JObject jsonMessage, String method)
-        {
-            con = Connect(url, xKey, method);
-            con.ContentLength = 0;
-            if (jsonMessage != null)
-            {
-                con.ContentLength = jsonMessage.ToString().Length;
-                var streamWriter = new StreamWriter(con.GetRequestStream());
-                streamWriter.Write(jsonMessage);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-
-            //Test de l'envoi
-            HttpWebResponse httpResponse = null;
-            int response = 0;
-            String responseString = null;
-            try
-            {
-                httpResponse = (HttpWebResponse)con.GetResponse();
-                response = 200;
-
-                //Lecture des donnees
-                StreamReader reader = new StreamReader(con.GetResponse().GetResponseStream());
-                responseString = reader.ReadToEnd();
-                httpResponse.Close();
-                reader.Close();
-            }
-            //Reception du signal
-            catch (WebException ex)
-            {
-                if (ex.Status == WebExceptionStatus.ProtocolError)
-                {
-                    httpResponse = (HttpWebResponse)ex.Response;
-                    response = (int)httpResponse.StatusCode;
-                }
-            }
-            Object[] result = { response, con, httpResponse, responseString };
-            return (result);
+            ((HttpWebResponse)result[2]).Close();
         }
     }
 }
